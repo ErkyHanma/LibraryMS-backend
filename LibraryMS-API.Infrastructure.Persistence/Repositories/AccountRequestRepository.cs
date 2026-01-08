4b1e1cdf6fc22c335846a7ecf1c9f2668f97b2ff
Erkyhanma <yenzel.bt@gmail.com>
2026-01-08 17:32:36 -0400

    feat: add entities repositories
    - Add BookRepository
    - Add BorrowRecordRepository
    - Add AccounRequestRepository
    - Add interfaces for all repositories
    - Add a generic repositories

---
+++ LibraryMS-API.Infrastructure.Persistence/Repositories/AccountRequestRepository.cs
@@ -0,0 +1,11 @@
+﻿using LibraryMS_API.Core.Domain.Entities;
+using LibraryMS_API.Infrastructure.Persistence.Contexts;
+using LibraryMS_API.Infrastructure.Persistence.Repositories.Base;
+
+namespace LibraryMS_API.Infrastructure.Persistence.Repositories
+{
+    public class AccountRequestRepository : GenericRepository<Book>
+    {
+        public AccountRequestRepository(LibraryMSContext context) : base(context) { }
+    }
+}
---
+++ LibraryMS-API.Infrastructure.Persistence/Repositories/BorrowRecordRepository.cs
@@ -0,0 +1,11 @@
+﻿using LibraryMS_API.Core.Domain.Entities;
+using LibraryMS_API.Infrastructure.Persistence.Contexts;
+using LibraryMS_API.Infrastructure.Persistence.Repositories.Base;
+
+namespace LibraryMS_API.Infrastructure.Persistence.Repositories
+{
+    public class BorrowRecordRepository : GenericRepository<Book>
+    {
+        public BorrowRecordRepository(LibraryMSContext context) : base(context) { }
+    }
+}
