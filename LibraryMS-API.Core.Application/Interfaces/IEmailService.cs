1863b64725156b06b82a94d057bc5f810431f4a0
Erkyhanma <yenzel.bt@gmail.com>
2026-01-10 10:40:45 -0400

    feat: add email service
    - add shared layer
    - implement email service

---
+++ LibraryMS-API.Core.Application/Interfaces/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using LibraryMS_API.Core.Application.Dtos.Email;
+
+namespace LibraryMS_API.Core.Application.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendAsync(EmailRequestDto emailRequestDto);
+    }
+}
---
+++ LibraryMS-API.Infrastructure.Shared/IOC/ServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using LibraryMS_API.Core.Application.Interfaces;
+using LibraryMS_API.Core.Domain.Settings;
+using LinkUp.Infrastructure.Shared.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibraryMS_API.Infrastructure.Shared.IOC
+{
+    public static class ServiceRegistration
+    {
+
+        public static void AddSharedLayerIOC(this IServiceCollection services, IConfiguration config)
+        {
+            #region Configurations
+            services.Configure<MailSettings>(config.GetSection("MailSettings"));
+            #endregion
+
+            #region Services IOC
+            services.AddScoped<IEmailService, EmailService>();
+            #endregion
+        }
+
+    }
+}
